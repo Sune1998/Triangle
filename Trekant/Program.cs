2eea6c4732ba6b0afa4d434437f6fa59883dfaeb
sune <sune1309@edu.easj.dk>
2021-05-25 16:59:56 +0200

    Konsol aplikation køre

--- Trekant/Program.cs
+++ Trekant/Program.cs
@@ -6,6 +6,11 @@ namespace Trekant
     {
         static void Main(string[] args)
         {
+            int sideA, sideB, sideC;
+            
+            Console.WriteLine("Check om din trekant er ligesidet, uligesidet eller ligebenet");
+            Console.WriteLine("\n\n");
+            
             
         }
     }
