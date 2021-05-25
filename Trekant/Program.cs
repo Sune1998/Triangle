acbeca529c31be726e4b9d9568eb3c97383ffeea
sune <sune1309@edu.easj.dk>
2021-05-25 17:08:37 +0200

    Bruger input virker

--- Trekant/Program.cs
+++ Trekant/Program.cs
@@ -11,7 +11,16 @@ namespace Trekant
             Console.WriteLine("Check om din trekant er ligesidet, uligesidet eller ligebenet");
             Console.WriteLine("\n\n");
             
+            Console.WriteLine("Intast venligst 1. side af din trekant:");
+            sideA = Convert.ToInt32(Console.ReadLine());
             
+            Console.WriteLine("Intast venligst 2. side af din trekant:");
+            sideB = Convert.ToInt32(Console.ReadLine());
+             
+            Console.WriteLine("Intast venligst 3. side af din trekant:");
+            sideC = Convert.ToInt32(Console.ReadLine());
+              
+              
         }
     }
 }
