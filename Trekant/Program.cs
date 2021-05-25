90a1f7151f31ceea4b01f6abaff7b17179169464
sune <sune1309@edu.easj.dk>
2021-05-25 17:34:36 +0200

    programmet virker nu. Det er wrapped i en while løkke således at man kan blive ved med at udregne trekanter. decimaltal er nu understøttede

--- Trekant/Program.cs
+++ Trekant/Program.cs
@@ -4,23 +4,45 @@ namespace Trekant
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            int sideA, sideB, sideC;
-            
-            Console.WriteLine("Check om din trekant er ligesidet, uligesidet eller ligebenet");
-            Console.WriteLine("\n\n");
-            
-            Console.WriteLine("Intast venligst 1. side af din trekant:");
-            sideA = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine("Intast venligst 2. side af din trekant:");
-            sideB = Convert.ToInt32(Console.ReadLine());
-             
-            Console.WriteLine("Intast venligst 3. side af din trekant:");
-            sideC = Convert.ToInt32(Console.ReadLine());
-              
-              
+            while (true)
+            {
+                
+                decimal sideA, sideB, sideC;
+
+                Console.WriteLine("Check om din trekant er ligesidet, uligesidet eller ligebenet");
+                Console.WriteLine("\n\n");
+
+                Console.WriteLine("Intast venligst 1. side af din trekant:");
+                sideA = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Intast venligst 2. side af din trekant:");
+                sideB = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Intast venligst 3. side af din trekant:");
+                sideC = Convert.ToDecimal(Console.ReadLine());
+
+
+                if (sideA == sideB && sideB == sideC)
+                {
+                    Console.WriteLine("Dette er en ligesidet trekant. \n");
+                }
+
+                else if (sideA == sideB || sideA == sideC || sideB == sideC)
+                {
+                    Console.WriteLine("Dette er en ligebenet trekant \n");
+                }
+
+                else
+                {
+                    Console.WriteLine("Dette er en uligesidet trekant \n\n");
+                }
+
+            }
+
         }
-    }
+        
+}
 }
