ba8d260cdbc2927f99b951bd149c8ecfd9da5436
sune <sune1309@edu.easj.dk>
2021-05-25 17:47:08 +0200

    flyttede trekantsudregner over i sin egen klasse.

--- Trekant/Program.cs
+++ Trekant/Program.cs
@@ -2,45 +2,14 @@
 
 namespace Trekant
 {
-    class Program
+    class Program 
     {
         
         static void Main(string[] args)
         {
-            while (true)
-            {
-                
-                decimal sideA, sideB, sideC;
-
-                Console.WriteLine("Check om din trekant er ligesidet, uligesidet eller ligebenet");
-                Console.WriteLine("\n\n");
-
-                Console.WriteLine("Intast venligst 1. side af din trekant:");
-                sideA = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Intast venligst 2. side af din trekant:");
-                sideB = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Intast venligst 3. side af din trekant:");
-                sideC = Convert.ToDecimal(Console.ReadLine());
-
-
-                if (sideA == sideB && sideB == sideC)
-                {
-                    Console.WriteLine("Dette er en ligesidet trekant. \n");
-                }
-
-                else if (sideA == sideB || sideA == sideC || sideB == sideC)
-                {
-                    Console.WriteLine("Dette er en ligebenet trekant \n");
-                }
-
-                else
-                {
-                    Console.WriteLine("Dette er en uligesidet trekant \n\n");
-                }
-
-            }
+           
+            Udregner udregner = new Udregner();
+            udregner.udregner();
 
         }
         
